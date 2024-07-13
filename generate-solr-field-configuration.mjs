deae9fbf9eff8781cad87ab1dbc774c119421e54
David <da70@nyu.edu>
2024-07-13 15:15:56 -0400

    generate-solr-field-configuration.mjs: write out basic main doc xpath to Solr field map

--- generate-solr-field-configuration.mjs
+++ generate-solr-field-configuration.mjs
@@ -1,5 +1,31 @@
-import { solrFields } from './v1-indexer-solr-fields.mjs';
+import { writeFileSync } from 'node:fs';
 
-Object.keys( solrFields ).sort().forEach( solrFieldName => {
-    const solrField = solrFields[ solrFieldName ];
+import { mainDocSolrFields } from './v1-indexer-solr-fields.mjs';
+import { indexAsConversion } from './index-as-conversion.mjs';
+
+const mainDocXpathToSolrFields = {};
+
+Object.keys( mainDocSolrFields ).sort().forEach( solrFieldName => {
+    const solrField = mainDocSolrFields[ solrFieldName ];
+    const suffixes = [];
+    const indexAsArray = solrField.indexAsArray;
+    if ( indexAsArray ) {
+        indexAsArray.forEach( indexAs => {
+            suffixes.push( ...indexAsConversion[ indexAs ].suffixes );
+        } );
+    }
+    const suffixedSolrFields =
+        suffixes.map( suffix => `${ solrFieldName }${ suffix }` );
+
+    if ( ! mainDocXpathToSolrFields[ solrField.xpath ] ) {
+        mainDocXpathToSolrFields[ solrField.xpath ] = {};
+        mainDocXpathToSolrFields[ solrField.xpath ].solrFields = [];
+    }
+    mainDocXpathToSolrFields[ solrField.xpath ].solrFields.push( ...suffixedSolrFields );
 } );
+
+writeFileSync(
+    'main-doc-xpath-to-solr-fields.json',
+    JSON.stringify( mainDocXpathToSolrFields, null, '    ' ),
+    { encoding : 'utf8' },
+);
