a809dd5d3bfbf0ec2e65f32b3a10318c72a89ea4
David <da70@nyu.edu>
2024-07-23 10:09:48 -0400

    v1-indexer-solr-fields.mjs: whitespace reformatting

--- v1-indexer-solr-fields.mjs
+++ v1-indexer-solr-fields.mjs
@@ -10,197 +10,197 @@ const mainDocSolrFields = {
             source : 'First token of <eadid>',
         },
     },
-    solrizer : {
-        composite: {
+    solrizer    : {
+        composite : {
             creator : {
-                xpathQueries: [
+                xpathQueries : [
                     "//origination[@label='creator']/corpname",
                     "//origination[@label='creator']/famname",
                     "//origination[@label='creator']/persname",
                 ],
-                process: 'Get elements for each xpath query in the order listed.' +
-                         ' Flatten returned node sets into one array, remove' +
-                         ' duplicates, and remove `nil` values.',
+                process      : 'Get elements for each xpath query in the order listed.' +
+                               ' Flatten returned node sets into one array, remove' +
+                               ' duplicates, and remove `nil` values.',
                 indexAsArray : [ DISPLAYABLE, FACETABLE ],
             },
-            name : {
-                xpathQueries: [
+            name    : {
+                xpathQueries : [
                     "//famname",
                     "//persname",
                     "//*[local-name()!='repository']/corpname",
                 ],
-                process: 'Get elements for each xpath query in the order listed.' +
-                         ' Flatten returned node sets into one array,' +
-                         ' remove `nil` values, and remove duplicates, and for ' +
-                         ' each element replace strings matching /\|\w{1}/' +
-                         ' (MARC subfield demarcators) with "--".',
+                process      : 'Get elements for each xpath query in the order listed.' +
+                               ' Flatten returned node sets into one array,' +
+                               ' remove `nil` values, and remove duplicates, and for ' +
+                               ' each element replace strings matching /\|\w{1}/' +
+                               ' (MARC subfield demarcators) with "--".',
                 indexAsArray : [ FACETABLE, SEARCHABLE ],
             },
             subject : {
-                xpathQueries: [
+                xpathQueries : [
                     '[archdesc[@level=\'collection\']/*[name() != \'dsc\']//subject',
                     '//*[local-name()=\'subject\' or local-name()=\'function\' or local-name() = \'occupation\']',
                 ],
-                process: 'For the first xpath query, create these Solr doc fields: ' +
-                         ' `subject_ssm`, `subject_teim`, `subject_dtim`, `subject_iim`.' +
-                         ' For the second xpath query, create the same set of' +
-                         ' Solr fields plus `subject_sim`, taking the node sets' +
-                         ' and flattening them into an array, remove `nil` values' +
-                         ' and duplicates.  For each element replace strings matching' +
-                         ' /\|\w{1}/ (MARC subfield demarcators) with "--".',
+                process      : 'For the first xpath query, create these Solr doc fields: ' +
+                               ' `subject_ssm`, `subject_teim`, `subject_dtim`, `subject_iim`.' +
+                               ' For the second xpath query, create the same set of' +
+                               ' Solr fields plus `subject_sim`, taking the node sets' +
+                               ' and flattening them into an array, remove `nil` values' +
+                               ' and duplicates.  For each element replace strings matching' +
+                               ' /\|\w{1}/ (MARC subfield demarcators) with "--".',
                 indexAsArray : [ FACETABLE, SEARCHABLE, DISPLAYABLE ],
             },
         },
-        nonXpath: {
-            dao: {
-                basename: 'dao',
-                source: 'Hardcoded "Online Access" if //dao is not empty',
+        nonXpath  : {
+            dao                       : {
+                basename     : 'dao',
+                source       : 'Hardcoded "Online Access" if //dao is not empty',
                 indexAsArray : [ FACETABLE ],
             },
-            formatArchivalCollection: {
-                basename: 'format',
-                source : [ 'Hardcoded "Archival Collection"' ],
+            formatArchivalCollection  : {
+                basename     : 'format',
+                source       : [ 'Hardcoded "Archival Collection"' ],
                 indexAsArray : [ FACETABLE, DISPLAYABLE ],
             },
-            formatDummyForMainDocSort: {
-                basename: 'format',
-                source : [ 'Hardcoded 0 (number, not string)' ],
+            formatDummyForMainDocSort : {
+                basename     : 'format',
+                source       : [ 'Hardcoded 0 (number, not string)' ],
                 indexAsArray : [ 'sortable' ],
             },
-            repository : {
-                source    : [ 'Derived from name of parent directory of EAD file' ],
+            repository                : {
+                source       : [ 'Derived from name of parent directory of EAD file' ],
                 indexAsArray : [ DISPLAYABLE, FACETABLE, STORED_STORABLE ],
             },
         },
-        xpath: {
-            abstract : {
-                xpath    : 'archdesc[@level=\'collection\']/did/abstract',
+        xpath     : {
+            abstract           : {
+                xpath        : 'archdesc[@level=\'collection\']/did/abstract',
                 indexAsArray : [ SEARCHABLE, DISPLAYABLE ],
             },
-            acqinfo : {
-                xpath    : 'archdesc[@level=\'collection\']/acqinfo/p',
+            acqinfo            : {
+                xpath        : 'archdesc[@level=\'collection\']/acqinfo/p',
                 indexAsArray : [ SEARCHABLE ],
             },
-            appraisal : {
-                xpath    : 'archdesc[@level=\'collection\']/appraisal/p',
+            appraisal          : {
+                xpath        : 'archdesc[@level=\'collection\']/appraisal/p',
                 indexAsArray : [ SEARCHABLE ],
             },
-            author : {
-                xpath    : 'filedesc/titlestmt/author',
+            author             : {
+                xpath        : 'filedesc/titlestmt/author',
                 indexAsArray : [ SEARCHABLE, DISPLAYABLE ],
             },
-            bioghist : {
-                xpath    : 'archdesc[@level=\'collection\']/bioghist/p',
+            bioghist           : {
+                xpath        : 'archdesc[@level=\'collection\']/bioghist/p',
                 indexAsArray : [ SEARCHABLE ],
             },
-            chronlist : {
-                xpath    : 'archdesc[@level=\'collection\']/*[name() != \'dsc\']//chronlist/chronitem//text()',
+            chronlist          : {
+                xpath        : 'archdesc[@level=\'collection\']/*[name() != \'dsc\']//chronlist/chronitem//text()',
                 indexAsArray : [ SEARCHABLE ],
             },
-            collection : {
-                xpath    : 'archdesc[@level=\'collection\']/did/unittitle',
+            collection         : {
+                xpath        : 'archdesc[@level=\'collection\']/did/unittitle',
                 indexAsArray : [ FACETABLE, DISPLAYABLE, SEARCHABLE ],
             },
-            corpname : {
-                xpath    : 'archdesc[@level=\'collection\']/*[name() != \'dsc\']//corpname',
+            corpname           : {
+                xpath        : 'archdesc[@level=\'collection\']/*[name() != \'dsc\']//corpname',
                 indexAsArray : [ SEARCHABLE, DISPLAYABLE ],
             },
-            creator : {
-                xpath    : 'archdesc[@level=\'collection\']/did/origination[@label=\'creator\']/*[#{creator_fields_to_xpath}]',
+            creator            : {
+                xpath        : 'archdesc[@level=\'collection\']/did/origination[@label=\'creator\']/*[#{creator_fields_to_xpath}]',
                 indexAsArray : [ SEARCHABLE, DISPLAYABLE ],
             },
-            custodhist : {
-                xpath    : 'archdesc[@level=\'collection\']/custodhist/p',
+            custodhist         : {
+                xpath        : 'archdesc[@level=\'collection\']/custodhist/p',
                 indexAsArray : [ SEARCHABLE ],
             },
-            ead : {
-                xpath : 'eadid',
+            ead                : {
+                xpath        : 'eadid',
                 indexAsArray : [ STORED_STORABLE ],
             },
-            famname : {
-                xpath    : 'archdesc[@level=\'collection\']/*[name() != \'dsc\']//famname',
+            famname            : {
+                xpath        : 'archdesc[@level=\'collection\']/*[name() != \'dsc\']//famname',
                 indexAsArray : [ SEARCHABLE, DISPLAYABLE ],
             },
-            function : {
-                xpath    : 'archdesc[@level=\'collection\']/*[name() != \'dsc\']//function',
+            function           : {
+                xpath        : 'archdesc[@level=\'collection\']/*[name() != \'dsc\']//function',
                 indexAsArray : [ SEARCHABLE, DISPLAYABLE ],
             },
-            genreform : {
-                xpath    : 'archdesc[@level=\'collection\']/*[name() != \'dsc\']//genreform',
+            genreform          : {
+                xpath        : 'archdesc[@level=\'collection\']/*[name() != \'dsc\']//genreform',
                 indexAsArray : [ SEARCHABLE, DISPLAYABLE ],
             },
-            geogname : {
-                xpath    : 'archdesc[@level=\'collection\']/*[name() != \'dsc\']//geogname',
+            geogname           : {
+                xpath        : 'archdesc[@level=\'collection\']/*[name() != \'dsc\']//geogname',
                 indexAsArray : [ SEARCHABLE, DISPLAYABLE ],
             },
-            langcode : {
-                xpath : 'archdesc[@level=\'collection\']/did/langmaterial/language/@langcode',
+            langcode           : {
+                xpath        : 'archdesc[@level=\'collection\']/did/langmaterial/language/@langcode',
                 indexAsArray : [ STORED_SEARCHABLE ],
             },
-            material_type : {
-                xpath: '//genreform',
-                process: 'For each element, replace strings matching /\|\w{1}/' +
-                         ' (MARC subfield demarcators) with "--",' +
-                         ' remove `nil` values, and remove duplicates.',
+            material_type      : {
+                xpath        : '//genreform',
+                process      : 'For each element, replace strings matching /\|\w{1}/' +
+                               ' (MARC subfield demarcators) with "--",' +
+                               ' remove `nil` values, and remove duplicates.',
                 indexAsArray : [ FACETABLE, DISPLAYABLE ],
             },
-            name : {
-                xpath    : 'archdesc[@level=\'collection\']/*[name() != \'dsc\']//name',
+            name               : {
+                xpath        : 'archdesc[@level=\'collection\']/*[name() != \'dsc\']//name',
                 indexAsArray : [ SEARCHABLE, DISPLAYABLE ],
             },
-            note : {
-                xpath    : 'archdesc[@level=\'collection\']/*[name() != \'dsc\']//note',
+            note               : {
+                xpath        : 'archdesc[@level=\'collection\']/*[name() != \'dsc\']//note',
                 indexAsArray : [ SEARCHABLE, DISPLAYABLE ],
             },
-            occupation : {
-                xpath    : 'archdesc[@level=\'collection\']/*[name() != \'dsc\']//occupation',
+            occupation         : {
+                xpath        : 'archdesc[@level=\'collection\']/*[name() != \'dsc\']//occupation',
                 indexAsArray : [ SEARCHABLE, DISPLAYABLE ],
             },
-            persname : {
-                xpath    : 'archdesc[@level=\'collection\']/*[name() != \'dsc\']//persname',
+            persname           : {
+                xpath        : 'archdesc[@level=\'collection\']/*[name() != \'dsc\']//persname',
                 indexAsArray : [ SEARCHABLE, DISPLAYABLE ],
             },
-            phystech : {
-                xpath    : 'archdesc[@level=\'collection\']/phystech/p',
+            phystech           : {
+                xpath        : 'archdesc[@level=\'collection\']/phystech/p',
                 indexAsArray : [ SEARCHABLE ],
             },
-            place : {
-                xpath: '//geogname',
-                process: 'For each element, replace strings matching /\|\w{1}/' +
-                         ' (MARC subfield demarcators) with "--",' +
-                         ' remove `nil` values, and remove duplicates.',
+            place              : {
+                xpath        : '//geogname',
+                process      : 'For each element, replace strings matching /\|\w{1}/' +
+                               ' (MARC subfield demarcators) with "--",' +
+                               ' remove `nil` values, and remove duplicates.',
                 indexAsArray : [ FACETABLE ],
             },
-            scopecontent : {
-                xpath    : 'archdesc[@level=\'collection\']/scopecontent/p',
+            scopecontent       : {
+                xpath        : 'archdesc[@level=\'collection\']/scopecontent/p',
                 indexAsArray : [ SEARCHABLE ],
             },
-            title : {
-                xpath    : 'archdesc[@level=\'collection\']/*[name() != \'dsc\']//title',
+            title              : {
+                xpath        : 'archdesc[@level=\'collection\']/*[name() != \'dsc\']//title',
                 indexAsArray : [ SEARCHABLE, DISPLAYABLE ],
             },
-            unitdate : {
-                xpath    : 'archdesc[@level=\'collection\']/did/unitdate[not(@type)]',
+            unitdate           : {
+                xpath        : 'archdesc[@level=\'collection\']/did/unitdate[not(@type)]',
                 indexAsArray : [ SEARCHABLE ],
             },
-            unitdate_bulk : {
-                xpath    : 'archdesc[@level=\'collection\']/did/unitdate[@type=\'bulk\']',
+            unitdate_bulk      : {
+                xpath        : 'archdesc[@level=\'collection\']/did/unitdate[@type=\'bulk\']',
                 indexAsArray : [ SEARCHABLE ],
             },
-            unitdate_normal : {
-                xpath    : 'archdesc[@level=\'collection\']/did/unitdate/@normal',
+            unitdate_normal    : {
+                xpath        : 'archdesc[@level=\'collection\']/did/unitdate/@normal',
                 indexAsArray : [ DISPLAYABLE, SEARCHABLE, FACETABLE ],
             },
             unitdate_inclusive : {
-                xpath    : 'archdesc[@level=\'collection\']/did/unitdate[@type=\'inclusive\']',
+                xpath        : 'archdesc[@level=\'collection\']/did/unitdate[@type=\'inclusive\']',
                 indexAsArray : [ SEARCHABLE ],
             },
-            unitid : {
-                xpath    : 'archdesc[@level=\'collection\']/did/unitid',
+            unitid             : {
+                xpath        : 'archdesc[@level=\'collection\']/did/unitid',
                 indexAsArray : [ SEARCHABLE, DISPLAYABLE ],
             },
-            unittitle : {
-                xpath    : 'archdesc[@level=\'collection\']/did/unittitle',
+            unittitle          : {
+                xpath        : 'archdesc[@level=\'collection\']/did/unittitle',
                 indexAsArray : [ SEARCHABLE, DISPLAYABLE ],
             },
         },
